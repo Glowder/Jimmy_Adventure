1af0a0c614ec4168c9037630df2b846c76a9cf9a
Glowder <maron19@hotmail.de>
2025-10-02 22:49:15 +0200

    Enhance PlayerStats class: add required XP calculation for leveling and improve instance creation logic

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
@@ -5,13 +6,21 @@ public class PlayerStats : MonoBehaviour
   public static PlayerStats instance;
   [SerializeField] Sprite playerPortrait;
   [SerializeField] string playerName;
+  [SerializeField]
+  int maxLevel = 50, currentLevel = 1,
+  maxXP, currentXP,
+  maxHP = 100, currentHP, strength = 3,
+  maxMP = 80, currentMP, intelligence = 5,
+  dexterity = 2,
+  physicalDEF = 2, magicalDEF = 4;
 
-
+  public List<int> requiredXPForEachLevel = new List<int>();
 
   // Start is called once before the first execution of Update after the MonoBehaviour is created
   void Start()
   {
     CreateInstance();
+    SetRequieredXPForEachLevel();
   }
 
   // Update is called once per frame
@@ -27,7 +36,7 @@ public class PlayerStats : MonoBehaviour
 
     Debug.Log("Player instance created from PlayerStats.");
   }
-    
+
   public string GetPlayerName()
   {
     return playerName;
@@ -36,5 +45,22 @@ public class PlayerStats : MonoBehaviour
   public Sprite GetPlayerPortrait()
   {
     return playerPortrait;
+  }
+
+  private void SetRequieredXPForEachLevel()
+  {
+    // Clears the list to avoid duplicates when relading the scene.
+    requiredXPForEachLevel.Clear();
+
+
+    for (int i = 0; i < maxLevel; i++)
+    {
+      // int xp = (int)(i + Mathf.Sqrt(i) * 17);
+      // int xp = (int)(Mathf.Pow(i, 2) * 15);   // z.B. quadratisch
+      // int xp = i * 100;                       // lineare Kurve
+      int xp = (int)(Mathf.Pow(1.15f, i) * 36); // exponentiell wachsend
+
+      requiredXPForEachLevel.Add(xp);
+    }
   }
 }
